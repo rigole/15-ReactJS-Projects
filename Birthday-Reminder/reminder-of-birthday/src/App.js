3d417bf73e4b7bdffd63543a766a167d44bdd0b5
rigole <foplacide@gmail.com>
2021-11-19 21:56:48 +0100

    creating the card in static

--- Birthday-Reminder/reminder-of-birthday/src/App.js
+++ Birthday-Reminder/reminder-of-birthday/src/App.js
@@ -1,8 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import data from "./data";
+import List from "./List";
 
 function App() {
-  return <h2>Birthday Reminder</h2>
+  const [people, setPeople] = useState(data)
+  return (
+      <main>
+        <section className='container'>
+          <h3> 0  Birthday today </h3>
+          <List people={people}/>
+          <button onClick={()=> console.log('you clicked me')}>Clear all</button>
+        </section>
+      </main>
+  )
 }
 
 export default App;
