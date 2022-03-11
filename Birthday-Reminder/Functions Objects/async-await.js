343e02ddd4e4107abc83c5c6387fe791d7f7ddfb
rigole <foplacide@gmail.com>
2022-03-11 11:19:28 +0100

    ending promises and starting Async-Await with ES8

---
+++ Birthday-Reminder/Functions Objects/async-await.js
@@ -0,0 +1,28 @@
+// ES8
+movePlayer(100, 'Left')
+.then(() => movePlayer(400, 'Left'))
+.then(() => movePlayer(10, 'Right'))
+.then(() => movePlayer(33, 'Left'))
+
+async function playerStart() {
+    const firstMove = await movePlayer(100, 'Left')
+    const second = await movePlayer(400, 'Left')
+    const third = await movePlayer(10, 'Right')
+    await movePlayer(330, 'Left')
+}
+
+async function fetchUsers() {
+    const resp = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await resp.json();
+    /*.then(resp => resp.json())
+    .then(console.log)*/
+
+    console.log(data)
+}
+
+const urls = [
+
+    'https://jsonplaceholder.typicode.com/users/',
+    'https://jsonplaceholder.typicode.com/posts/',
+    'https://jsonplaceholder.typicode.com/albums/'
+]
