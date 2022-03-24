2db996c431ba7ec7ad2ab3311c685d1ff72d67fb
rigole <foplacide@gmail.com>
2022-03-24 08:26:38 +0100

    try catch

--- Birthday-Reminder/Error Handling/tryCatch.js
+++ Birthday-Reminder/Error Handling/tryCatch.js
@@ -7,7 +7,37 @@ function fail() {
     } catch (error) {
 
         console.log(error.name)
+    } finally {
+        console.log("Still good")
+        return "This was a fail"
     }
 }
 
 fail()
+
+
+try {
+
+    try {
+        something()
+
+    } catch (err) {
+
+        throw new Error(err)
+    }
+    
+} catch (error) {
+  
+    console.log('got it', error)
+
+}
+
+try {
+
+    setTimeout(function () {
+            fakevariable;
+    }, 1000)
+    
+} catch (error) {
+    console.log('got it', error)
+}
